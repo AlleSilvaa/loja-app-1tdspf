66a938c7b975239b0b8ae31cdc146ffbf2d2060c
AlleSilvaa <allessonaugusto10@gmail.com>
2023-10-19 11:02:48 -0300

    projeto destinado a entrega 4 do challenger

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,19 +6,16 @@ export default function Home() {
   return (
     
     <>
-      <h1>HOME</h1>
 
-      <h2>MEUS PRODUTOS</h2>
-      <ul>
-        <li><Link href="/produtos/calca">CALÇA</Link></li>
-        <li><Link href="/produtos/meia">MEIA</Link></li>
-        <li><Link href="/produtos/camisa">CAMISA</Link></li>
-        <li><Link href="/produtos/tenis">TÊNIS</Link></li>
-      </ul>
+      <h1>HOME</h1>
+      
+      <div>
 
       <figure>
           <Image src="/img/black-friday.jpg" alt="flore" height={500} width={500}/>
       </figure>
+
+      </div>
 
     </>
 
